116dc92778ffc2a9cdda418dffd3e8cb0331e5ec
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-02-13 15:35:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ try:
     data_8020 = data.columns['Maquina','Falla','Duration_Hrs']
     lista_maquinas = data_8020.sort_values(by="Maquina", ascending = True)
     lista_maquinas = lista_maquinas['Maquina'].unique()
-    print(lista_maquinas)
+    st.write(lista_maquinas)
     #maquina_pareto = st.selectbox(
     #    "Seleccionar Máquina", options = lista_maquinas)
 
