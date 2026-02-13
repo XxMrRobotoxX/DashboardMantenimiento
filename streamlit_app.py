7b7ad29d9d5e6a257b4265ef9d0341ed771cca53
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-02-13 15:41:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,10 +104,10 @@ try:
 
     col5, col6 = st.columns(2)
 
-    data_8020 = data['Maquina','Falla']
+    #df_pareto = data['Maquina','Falla']
     #lista_maquinas = data_8020.sort_values(by="Maquina", ascending = True)
     #lista_maquinas = lista_maquinas['Maquina'].unique()
-    st.write(data_8020)
+    st.write(data)
     #maquina_pareto = st.selectbox(
     #    "Seleccionar Máquina", options = lista_maquinas)
 
