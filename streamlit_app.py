2be3826f029b05b7027c8b4e6bfe77a7134a1f85
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-02-13 16:54:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,7 +111,7 @@ try:
     col5, col6 = st.columns(2)
 
     df_pareto = data[['Maquina','Falla','Duration_Hrs']]
-    lista_maquinas = data_maquinas['Maquina']
+    lista_maquinas = data_maquinas[['ID']]
     #lista_maquinas = lista_maquinas['Maquina'].unique()
     #st.write(lista_maquinas)
     maquina_pareto = st.selectbox(
