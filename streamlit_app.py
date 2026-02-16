733c07d7ca559d4472b8b7c13dfe1d47c516e030
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-02-16 10:59:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,11 +99,11 @@ try:
         mttr_df = mttr_df.sort_values(by="Cantidad_Fallas", ascending=True)
         st.subheader("Frecuencia Fallas")
         fig2 = px.bar(mttr_df,
-                     x="Cantidad_Fallas",
+                     x="Cantidad Fallas",
                      y="Maquina",
                      text_auto='.0f',
                      title="Cantidad de fallas",
-                     color="Cantidad_Fallas",
+                     color="Cantidad Fallas",
                      color_continuous_scale="Reds",
                      orientation='h')
         st.plotly_chart(fig2, use_container_widht=True)
