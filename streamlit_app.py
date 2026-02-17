1a58dc80ab397c5ae8b60e4559eeb5cd49f1520b
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-02-17 12:44:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,9 @@ try:
 
     with col5:
         st.subheader("Diagrama de pareto 80-20")
-        fig3 = px.bar(
+        fig3 = px.bar(df_pareto_filtered,
+                      x='Falla',
+                      y='Duration_Hrs'
                      )
         
         st.plotly_chart(fig3, use_container_width=True)
