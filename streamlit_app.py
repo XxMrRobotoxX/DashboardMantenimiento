4ecbbc8f3f7abbcfd2c922a614aae6fba347efae
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-02-17 11:18:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,7 +119,7 @@ try:
 
     df_pareto_filtered = df_pareto[df_pareto['Maquina'] == maquina_pareto]
 
-    st.write(df_pareto_filtered.groupby(by='Maquina', ascending=False))
+    st.write(df_pareto_filtered.groupby('Maquina'))
 
     #with col5:
         #st.subheader("Diagrama de pareto 80-20")
