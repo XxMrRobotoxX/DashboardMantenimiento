e760ffea736a288139a9c26d8de9af5d10f0a065
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-02-26 12:55:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,14 +75,14 @@ try:
         mttr_df = mttr_df.sort_values(by="MTTR (Horas)", ascending=False)
         mtbf_df_2 = df_filtered.groupby('Maquina')['Duration_Hrs'].agg(['sum','count']).reset_index()
         mtbf_df_2.columns = ['Maquina','Tiempo muerto','CantidadFallas']
-        mtbf_df_end = mtbf_df.merge(mtbf_df_2, on = 'Maquina', how = 'left')
+        #mtbf_df_end = mtbf_df.merge(mtbf_df_2, on = 'Maquina', how = 'left')
     else:
         mttr_df = df_filtered.groupby("Maquina")["Duration_Hrs"].agg(['mean', 'count']).reset_index()
         mttr_df.columns = ["Maquina", "MTTR (Horas)", "Cantidad_Fallas"]
         mttr_df = mttr_df.sort_values(by="MTTR (Horas)", ascending=False)
         mtbf_df_2 = df_filtered.groupby('Maquina')['Duration_Hrs'].agg(['sum','count']).reset_index()
         mtbf_df_2.columns = ['Maquina','Tiempo muerto','CantidadFallas']
-        mtbf_df_end = mtbf_df.merge(mtbf_df_2, on = 'Maquina', how = 'left')
+        #mtbf_df_end = mtbf_df.merge(mtbf_df_2, on = 'Maquina', how = 'left')
     
     # MTTR = Suma de tiempo de reparación / Número de intervenciones
     #mttr_df = df_filtered.groupby("Maquina")["Duration_Hrs"].agg(['mean', 'count']).reset_index()
