f84a1fbc29a5b761baad05a42adf5b21d648b9dd
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-03-05 12:52:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,8 +84,8 @@ try:
     # --- CÁLCULO DE MTTR ---
 
     crit_filtred = st.toggle('Ver Máquinas Críticas')
-
-     if crit_filtred:
+    
+    if crit_filtred:
         df_filtered = df_filtered[df_filtered["Maquina"].isin(criticas)]
         mttr_df = df_filtered.groupby("Maquina")["Duration_Hrs"].agg(['mean', 'count', 'sum']).reset_index()
         mttr_df.columns = ["Maquina", "MTTR (Horas)", "Cantidad_Fallas",'Tiempo Muerto']
