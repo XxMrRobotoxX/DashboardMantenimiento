d1ff4f1953e34d709686dc8f8ed1f4c685c61f7c
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-03-05 11:44:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,9 +58,15 @@ try:
         mtbf_df = data_prog.groupby('Maquina')['minProg'].sum()
         
     else:
+        date_start = date_filter[0].strftime('%d/%m/%Y')
+        dia = date_filter[1].day
+        mes = date_filter[1].month
+        año = date_filter[1].year
+        date_end = date_filter[1].strftime('%d/%m/%Y')
         date_max = data_prog['Fecha'].max()
         df_filtered = data[data["Maquina"].isin(maquinas)]
         df_filtered['FechaInicio'] = pd.to_datetime(df_filtered['FechaInicio'], format = '%d/%m/%Y')
+        data_rnd = df_filtered['FechaInicio'].iloc[0]
         df_filtered = df_filtered[(df_filtered["Estatus"] == "Cerrada") & (df_filtered["CausoParo"] == "Si") & (df_filtered['FechaInicio'].between(date_filter[0],date_filter[1],inclusive='both'))]
         df_filtered_mtbf  = df_filtered[(df_filtered["Estatus"] == "Cerrada") & (df_filtered["CausoParo"] == "Si") & (df_filtered['FechaInicio'].between(date_start,datetime.strptime(date_max, '%d/%m/%Y'),inclusive='both'))]
         mtbf_df = data_prog[data_prog['Fecha'].between(date_start, date_end, inclusive = 'both')]
@@ -270,7 +276,7 @@ try:
     st.write(df_filtered['Duration_Hrs'].sum())
     st.write(date_end)
     st.write(date_max)
-    #st.write(mtbf_df_end)
+    st.write(date_rnd)
     with st.expander("Ver datos completos"):
         st.write(df_filtered)
         st.write(df_filtered_mtbf)
