1291d8f955589a45882ca467577805672e8424cf
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-03-11 11:15:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -236,7 +236,8 @@ try:
 
     with col7:
         #cant_falla_df = df_pareto_filtered.gropupby('Falla')['Falla'].agg(['count'])
-        st.write(df_pareto)
+        cant_falla_df = df_pareto.groupby('Falla')['Falla'].agg(['count'])
+        st.write(cant_falla_df)
 
     col8, col9 = st.columns(2)
 
