be24e376c8605cd45a646334b4f07bc206a17c8f
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-04-29 08:03:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,17 +168,17 @@ try:
     with col5:
         mttr_df = mttr_df.sort_values(by="Cantidad_Fallas", ascending=True)
         st.subheader("Frecuencia Fallas")
-        #fig2 = px.bar(mttr_df,
-        #             x="Cantidad_Fallas",
-        #             y="Maquina",
-        #             text_auto='.0f',
-        #             title="Cantidad de fallas",
-        #             color="Cantidad_Fallas",
-        #             color_continuous_scale="Reds",
-        #             orientation='h')
-        #st.plotly_chart(fig2, use_container_widht=True)
+        fig2 = px.bar(mttr_df,
+                     x="Cantidad_Fallas",
+                     y="Maquina",
+                     text_auto='.0f',
+                     title="Cantidad de fallas",
+                     color="Cantidad_Fallas",
+                     color_continuous_scale="Reds",
+                     orientation='h')
+        st.plotly_chart(fig2, use_container_widht=True)
 
-        st.write(mttr_df)
+        #st.write(mttr_df)
 
     df_pareto = df_filtered[['Maquina','Falla','Duration_Hrs']]
     lista_maquinas = data_maquinas[['ID']]
