91ebe4e39215b945571cf7bd0614abd609fe8d1f
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-04-30 15:50:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,199 +127,30 @@ try:
 
     # --- VISUALIZACIÓN ---
 
+    tab1, tab2, tab3, tab4 = st.tabs(['KPI Globales','MTTR','MTBF','Datos'])
 
-    col1, col2, col3 = st.columns(3)
+    with tab1:
+        col1, col2, col3 = st.columns(3)
 
-    #total_mttr = mttr_df['MTTR (Horas)'].mean()
-    total_mttr = df_filtered['Duration_Hrs'].sum()/len(df_filtered)
-    meta_mttr = 1.3
-    delta_mttr = total_mttr - meta_mttr
-
-    mtbf_global = ((total_programado/60)-total_tm)/total_fallas
-    meta_mtbf = 80
-    delta_mtbf = mtbf_global - meta_mtbf
+        #total_mttr = mttr_df['MTTR (Horas)'].mean()
+        total_mttr = df_filtered['Duration_Hrs'].sum()/len(df_filtered)
+        meta_mttr = 1.3
+        delta_mttr = total_mttr - meta_mttr
     
-    with col1:
-        st.metric("MTTR Global (Horas)", f"{total_mttr:.2f}", f"{delta_mttr:.2f}", delta_color = "inverse")
-
-    with col2:
-        st.metric("Total Intervenciones", len(df_filtered))
+        mtbf_global = ((total_programado/60)-total_tm)/total_fallas
+        meta_mtbf = 80
+        delta_mtbf = mtbf_global - meta_mtbf
+        
+        with col1:
+            st.metric("MTTR Global (Horas)", f"{total_mttr:.2f}", f"{delta_mttr:.2f}", delta_color = "inverse")
     
-    col4, col5 = st.columns([2, 1])
+        with col2:
+            st.metric("Total Intervenciones", len(df_filtered))
     
-    with col3:
-        st.metric("MTBF Global (Horas)", f"{mtbf_global:.2f}", f"{delta_mtbf:.2f}")
-
-
-    with col4:
-        st.subheader("MTTR por Máquina")
-        fig = px.bar(mttr_df, 
-                     x="Maquina", 
-                     y="MTTR (Horas)", 
-                     text_auto='.2f',
-                     title="Tiempo Medio de Reparación (Horas)",
-                     color="MTTR (Horas)",
-                     color_continuous_scale="Reds")
-
-        fig.add_hline(y=meta_mttr, line_dash="dash", line_color="green", annotation_text="Meta MTTR")
-        st.plotly_chart(fig, use_container_width=True)
-
-
-    with col5:
-        mttr_df = mttr_df.sort_values(by="Cantidad_Fallas", ascending=True)
-        st.subheader("Frecuencia Fallas")
-        fig2 = px.bar(mttr_df,
-                     x="Cantidad_Fallas",
-                     y="Maquina",
-                     text_auto='.0f',
-                     title="Cantidad de fallas",
-                     color="Cantidad_Fallas",
-                     color_continuous_scale="Reds",
-                     orientation='h')
-        st.plotly_chart(fig2, use_container_width=True)
-
-        #st.write(mttr_df)
-
-    df_pareto = df_filtered[['Maquina','Falla','Duration_Hrs']]
-    lista_maquinas = data_maquinas[['ID']]
-    maquina_pareto = st.selectbox(
-        "Seleccionar Máquina", options = lista_maquinas)
-
-    col6, col7 = st.columns(2)
-    
-    with col6:
-    
-        df_pareto_filtered = df_pareto[df_pareto['Maquina'] == maquina_pareto]
-        df_pareto_filtered = df_pareto_filtered.groupby('Falla')['Duration_Hrs'].sum().sort_values(ascending=False).reset_index()
-        df_pareto_filtered['PorcentajeAcum'] = df_pareto_filtered['Duration_Hrs'].cumsum()/df_pareto_filtered['Duration_Hrs'].sum()*100
         
-        st.subheader("Diagrama de pareto 80-20")
+        with col3:
+            st.metric("MTBF Global (Horas)", f"{mtbf_global:.2f}", f"{delta_mtbf:.2f}")
 
-        fig3 = go.Figure()
-        
-        # Añadir Barras (Eje Y primario)
-        fig3.add_trace(
-            go.Bar(
-                x=df_pareto_filtered['Falla'],
-                y=df_pareto_filtered['Duration_Hrs'],
-                name='Duración (Hrs)',
-                marker=dict(
-                    color=df_pareto_filtered['Duration_Hrs'],
-                    colorscale='Reds',
-                    showscale=False
-                )
-            )
-        )
-        
-        # Añadir Línea de Porcentaje (Eje Y secundario)
-        fig3.add_trace(
-            go.Scatter(
-                x=df_pareto_filtered['Falla'],
-                y=df_pareto_filtered['PorcentajeAcum'],
-                name='Porcentaje Acumulado',
-                mode='lines+markers',
-                line=dict(color='#EF553B', width=3),
-                yaxis='y2' # Indicamos que use el segundo eje
-            )
-        )
-        
-        # 3. Configuración del Layout para el eje secundario
-        fig3.update_layout(
-            title='Análisis de Fallas (Pareto)',
-            xaxis=dict(title='Falla'),
-            yaxis=dict(
-                title='Duración (Hrs)',
-                side='left'
-            ),
-            yaxis2=dict(
-                title='Porcentaje Acumulado (%)',
-                side='right',
-                overlaying='y',
-                range=[0, 105], # Rango de 0 a 100%
-                ticksuffix='%'
-            ),
-            legend=dict(x=0.8, y=1.1, orientation='h'),
-            template='plotly_dark' # Estilo oscuro para que combine con tu imagen
-        )
-        
-        st.plotly_chart(fig3, use_container_width=True)
-
-
-    with col7:
-        #cant_falla_df = df_pareto_filtered.gropupby('Falla')['Falla'].agg(['count'])
-        cant_falla_df = df_pareto[df_pareto['Maquina'] == maquina_pareto]
-        cant_falla_df = cant_falla_df.groupby('Falla', as_index=False).size()
-        cant_falla_df = cant_falla_df.rename(columns={'size':'count'})
-        cant_falla_df = cant_falla_df.sort_values(by='count', ascending = False)
-        #st.write(cant_falla_df)
-        st.subheader('Frecuencia de fallas por Máquina')
-        
-        fig5 = go.Figure()
-        
-        # Añadir Barras (Eje Y primario)
-        fig5.add_trace(
-            go.Bar(
-                x=cant_falla_df['Falla'],
-                y=cant_falla_df['count'],
-                name='Frecuencia de fallas por Máquina',
-                marker=dict(
-                    color=cant_falla_df['count'],
-                    colorscale='Reds',
-                    showscale=False
-                )
-            )
-        )
-
-        fig5.update_layout(
-            title='Frecuencia de fallas por Máquina',
-            xaxis=dict(title='Máquina'),
-            yaxis=dict(
-                title='Cantidad de fallas',
-                side='left'
-            )
-        )
-        st.plotly_chart(fig5, use_container_width=True)
-        
-    col8, col9 = st.columns(2)
-
-    with col8:
-        st.subheader('MTBF por Máquina')
-        
-        fig4 = go.Figure()
-        
-        # Añadir Barras (Eje Y primario)
-        fig4.add_trace(
-            go.Bar(
-                x=mtbf_df_end['Maquina'],
-                y=mtbf_df_end['MTBF (Horas)'],
-                name='MTBF (Horas)',
-                text=mtbf_df_end['MTBF (Horas)'],
-                textposition='auto',
-                texttemplate='%{y:.2f}',
-                marker=dict(
-                    color=mtbf_df_end['MTBF (Horas)'],
-                    colorscale='Reds',
-                    showscale=False
-                )
-            )
-        )
-
-        fig4.update_layout(
-            title='Tiempo medio entre fallas (Horas)',
-            xaxis=dict(title='Máquina'),
-            yaxis=dict(
-                title='MTBF (Horas)',
-                side='left'
-            )
-        )
-        fig4.add_hline(y=meta_mtbf, line_dash="dash", line_color="green", annotation_text="Meta MTBF")
-        st.plotly_chart(fig4, use_container_width=True)
-
-    with col9:
-
-        tab1, tab2 = st.tabs(
-            ['MTTR', 'MTBF'], default = 'MTTR'
-        )
 
         options = st.multiselect(
                 "Selecciona las semanas a graficar:",
@@ -327,7 +158,9 @@ try:
                 default = [max(data['Semana']), max(data['Semana'])-1, max(data['Semana'])-2, max(data['Semana'])-3]
             )
         
-        with tab1:
+        col4, col5 = st.columns([2, 1])
+
+        with col4:
             df_week_end = df_week_end[df_week_end['Semana'].isin(options)]
     
             fig6 = go.Figure()
@@ -361,7 +194,7 @@ try:
             fig6.add_hline(y=meta_mttr, line_dash="dash", line_color="green", annotation_text="Meta MTTR")
             st.plotly_chart(fig6, use_container_width=True)
 
-        with tab2:
+        with col5:
             fig7 = go.Figure()
             
             # Añadir Barras (Eje Y primario)
@@ -393,6 +226,194 @@ try:
             fig7.add_hline(y=meta_mtbf, line_dash="dash", line_color="green", annotation_text="Meta MTBF")
             st.plotly_chart(fig7, use_container_width=True)
 
+
+    with tab2:
+        col6, col7 = st.columns([2,1])
+
+        with col6:
+            st.subheader("MTTR por Máquina")
+            fig = px.bar(mttr_df, 
+                         x="Maquina", 
+                         y="MTTR (Horas)", 
+                         text_auto='.2f',
+                         title="Tiempo Medio de Reparación (Horas)",
+                         color="MTTR (Horas)",
+                         color_continuous_scale="Reds")
+    
+            fig.add_hline(y=meta_mttr, line_dash="dash", line_color="green", annotation_text="Meta MTTR")
+            st.plotly_chart(fig, use_container_width=True)
+    
+    
+        with col7:
+            mttr_df = mttr_df.sort_values(by="Cantidad_Fallas", ascending=True)
+            st.subheader("Frecuencia Fallas")
+            fig2 = px.bar(mttr_df,
+                         x="Cantidad_Fallas",
+                         y="Maquina",
+                         text_auto='.0f',
+                         title="Cantidad de fallas",
+                         color="Cantidad_Fallas",
+                         color_continuous_scale="Reds",
+                         orientation='h')
+            st.plotly_chart(fig2, use_container_width=True)
+
+        df_pareto = df_filtered[['Maquina','Falla','Duration_Hrs']]
+    lista_maquinas = data_maquinas[['ID']]
+    maquina_pareto = st.selectbox(
+        "Seleccionar Máquina", options = lista_maquinas)
+
+    col8, col9 = st.columns(2)
+    
+        with col8:
+        
+            df_pareto_filtered = df_pareto[df_pareto['Maquina'] == maquina_pareto]
+            df_pareto_filtered = df_pareto_filtered.groupby('Falla')['Duration_Hrs'].sum().sort_values(ascending=False).reset_index()
+            df_pareto_filtered['PorcentajeAcum'] = df_pareto_filtered['Duration_Hrs'].cumsum()/df_pareto_filtered['Duration_Hrs'].sum()*100
+            
+            st.subheader("Diagrama de pareto 80-20")
+    
+            fig3 = go.Figure()
+            
+            # Añadir Barras (Eje Y primario)
+            fig3.add_trace(
+                go.Bar(
+                    x=df_pareto_filtered['Falla'],
+                    y=df_pareto_filtered['Duration_Hrs'],
+                    name='Duración (Hrs)',
+                    marker=dict(
+                        color=df_pareto_filtered['Duration_Hrs'],
+                        colorscale='Reds',
+                        showscale=False
+                    )
+                )
+            )
+            
+            # Añadir Línea de Porcentaje (Eje Y secundario)
+            fig3.add_trace(
+                go.Scatter(
+                    x=df_pareto_filtered['Falla'],
+                    y=df_pareto_filtered['PorcentajeAcum'],
+                    name='Porcentaje Acumulado',
+                    mode='lines+markers',
+                    line=dict(color='#EF553B', width=3),
+                    yaxis='y2' # Indicamos que use el segundo eje
+                )
+            )
+            
+            # 3. Configuración del Layout para el eje secundario
+            fig3.update_layout(
+                title='Análisis de Fallas (Pareto)',
+                xaxis=dict(title='Falla'),
+                yaxis=dict(
+                    title='Duración (Hrs)',
+                    side='left'
+                ),
+                yaxis2=dict(
+                    title='Porcentaje Acumulado (%)',
+                    side='right',
+                    overlaying='y',
+                    range=[0, 105], # Rango de 0 a 100%
+                    ticksuffix='%'
+                ),
+                legend=dict(x=0.8, y=1.1, orientation='h'),
+                template='plotly_dark' # Estilo oscuro para que combine con tu imagen
+            )
+            
+            st.plotly_chart(fig3, use_container_width=True)
+    
+    
+        with col9:
+            #cant_falla_df = df_pareto_filtered.gropupby('Falla')['Falla'].agg(['count'])
+            cant_falla_df = df_pareto[df_pareto['Maquina'] == maquina_pareto]
+            cant_falla_df = cant_falla_df.groupby('Falla', as_index=False).size()
+            cant_falla_df = cant_falla_df.rename(columns={'size':'count'})
+            cant_falla_df = cant_falla_df.sort_values(by='count', ascending = False)
+            #st.write(cant_falla_df)
+            st.subheader('Frecuencia de fallas por Máquina')
+            
+            fig5 = go.Figure()
+            
+            # Añadir Barras (Eje Y primario)
+            fig5.add_trace(
+                go.Bar(
+                    x=cant_falla_df['Falla'],
+                    y=cant_falla_df['count'],
+                    name='Frecuencia de fallas por Máquina',
+                    marker=dict(
+                        color=cant_falla_df['count'],
+                        colorscale='Reds',
+                        showscale=False
+                    )
+                )
+            )
+    
+            fig5.update_layout(
+                title='Frecuencia de fallas por Máquina',
+                xaxis=dict(title='Máquina'),
+                yaxis=dict(
+                    title='Cantidad de fallas',
+                    side='left'
+                )
+            )
+            st.plotly_chart(fig5, use_container_width=True)
+
+    with tab3:
+        col8, col9 = st.columns(2)
+
+        with col8:
+            st.subheader('MTBF por Máquina')
+            
+            fig4 = go.Figure()
+            
+            # Añadir Barras (Eje Y primario)
+            fig4.add_trace(
+                go.Bar(
+                    x=mtbf_df_end['Maquina'],
+                    y=mtbf_df_end['MTBF (Horas)'],
+                    name='MTBF (Horas)',
+                    text=mtbf_df_end['MTBF (Horas)'],
+                    textposition='auto',
+                    texttemplate='%{y:.2f}',
+                    marker=dict(
+                        color=mtbf_df_end['MTBF (Horas)'],
+                        colorscale='Reds',
+                        showscale=False
+                    )
+                )
+            )
+    
+            fig4.update_layout(
+                title='Tiempo medio entre fallas (Horas)',
+                xaxis=dict(title='Máquina'),
+                yaxis=dict(
+                    title='MTBF (Horas)',
+                    side='left'
+                )
+            )
+            fig4.add_hline(y=meta_mtbf, line_dash="dash", line_color="green", annotation_text="Meta MTBF")
+            st.plotly_chart(fig4, use_container_width=True)
+
+
+    with tab4:
+        with st.expander("Ver datos completos"):
+        st.write(df_filtered)
+        st.write(df_week_mtbf)
+
+        
+        
+
+
+
+
+
+
+
+
+        
+
+        
+
+            
         
     
         
@@ -402,9 +423,7 @@ try:
     #data_prog = data_prog.groupby(['Maquina','Fecha'])['minProg'].sum()
 
     
-    with st.expander("Ver datos completos"):
-        st.write(df_filtered)
-        st.write(df_week_mtbf)
+    
 
 except Exception as e:
     st.error("Error al cargar los datos. Verifica que el enlace de Google Sheets sea correcto y público.")
