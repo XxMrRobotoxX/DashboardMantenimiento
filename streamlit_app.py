69b2261ae6ef5ec657b46780590d6d2028a4a0e3
XxMrRobotoxX <126634715+XxMrRobotoxX@users.noreply.github.com>
2026-05-22 09:41:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -405,7 +405,7 @@ try:
     
     with st.expander("Ver datos completos"):
         st.write(df_filtered)
-        st.write(df_pareto)
+        st.write(df_pareto_test)
 
 except Exception as e:
     st.error("Error al cargar los datos. Verifica que el enlace de Google Sheets sea correcto y público.")
